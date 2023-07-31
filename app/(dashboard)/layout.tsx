90b682e9d6317f2d7ebf2de2b65203515f976300
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2023-07-31 14:14:28 -0400

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -13,8 +13,6 @@ const DashboardLayout = async ({
 
   return ( 
     <div className="h-full relative">
-      <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
-<script src="https://mediafiles.botpress.cloud/82b53c5e-9cd1-4abb-8078-3602d532d0b0/webchat/config.js" defer></script>
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       </div>
