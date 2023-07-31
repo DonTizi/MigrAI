00dd0b18e846ee8fc877c1e5a84afcfe457996f8
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2023-07-31 10:52:00 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
 
   <ClerkProvider>
     <html lang="en">
-      <Script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="74bd671a-db9d-4aad-aa9b-776827ce6afe";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</Script>
       <body className={inter.className}>
 
 
