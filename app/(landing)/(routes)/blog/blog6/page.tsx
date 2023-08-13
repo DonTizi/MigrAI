cb5d633cb8f7e87191c0b23a0619a2bee081fcbf
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2023-08-13 00:59:36 -0400

    Update page.tsx

--- app/(landing)/(routes)/blog/blog6/page.tsx
+++ app/(landing)/(routes)/blog/blog6/page.tsx
@@ -4,7 +4,7 @@ import { UserLogo } from "@/components/user-logo";
 import { BotAvatar } from "@/components/bot-avatar";
 
 const relatedBlogs = [
-    { title: "Process Simulation Tool", link: "/blog/blog5" },
+    { title: "Process Simulation Tool Guide", link: "/blog/blog5" },
     { title: "Immigration Explainer Tool", link: "/blog/blog4" },
     { title: "Understanding the Canadian Experience", link: "/blog/blog3" },
 ];
