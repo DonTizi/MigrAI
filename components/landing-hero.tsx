dfb8601c10ed6b50bc1629ff8a9d438738295aca
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2023-08-13 12:04:01 -0400

    Update landing-hero.tsx

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,12 +12,12 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
+        <h1>Immigration AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
           <TypewriterComponent
             options={{
               strings: [
-                "Immigration.",
+                "Explanation.",
                 "Documentation.",
                 "Interviews.",
                 "Simulations."
