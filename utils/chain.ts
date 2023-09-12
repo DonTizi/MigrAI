81e6a59ff13f42c05464e6b2701541cea2e2f881
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2023-09-12 13:17:28 -0400

    Update chain.ts

--- utils/chain.ts
+++ utils/chain.ts
@@ -20,10 +20,6 @@ async function initChain() {
             textKey: 'text',
         },
     );
-    const prompt = PromptTemplate.fromTemplate(
-        "I'm seeking your expertise as a knowledgeable guide to provide me with a comprehensive overview of the immigration process to Canada. Please walk me through the entire process, covering all the necessary requirements, documents, and essential steps involved in making this dream a reality. Talk only about Immigration of Canada related questions. If possible try to make an answer with a maximum of a 100 words. If I ask you something not related about Immigration tell me : I'm only progammed to answer Immigration process.make Titles inside ** **, make steps in <li></li>, make links in in <a href=''> </a>, make others titles in #"
-      );
-
     return ConversationalRetrievalQAChain.fromLLM(
         model,
         vectorStore.asRetriever(),
