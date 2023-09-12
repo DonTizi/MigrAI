1fec9f3d01bd97691a8f2a4348c561c39897792b
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2023-09-12 14:52:29 -0400

    2023

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    staticPageGenerationTimeout: 100 // Set timeout to 100 seconds
+    staticPageGenerationTimeout: 90 // Set timeout to 100 seconds
 
 }
 
